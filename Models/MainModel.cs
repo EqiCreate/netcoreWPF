b962766984313d6097cb6766aa943260aabb5b7e
1245994915@qq.com <1245994915@qq.com>
2022-11-19 19:16:45 +0800

    init

---
+++ Models/MainModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace netcoreWPF.Models
+{
+    public class MainModel
+    {
+        public MainModel()
+        {
+            // noq.
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace netcoreWPF.ViewModels
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            // noq.
+        }
+    }
+}
